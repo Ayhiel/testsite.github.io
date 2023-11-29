57024e9ed41a5c863839979d54ce9b7103f2178f
Ayhiel <152348799+Ayhiel@users.noreply.github.com>
2023-11-29 11:24:07 +0800

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+<body class="bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand" href="#!" style="font-weight: 500;">GradeSys</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Dashboard</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Sign Up</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+        <!-- Header-->
+        <header class="bg-dark">
+            <div class="container px-sm-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">GradeSys</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">Monitor you progress!</p>
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="container-fluid text-light">
+            <section class="py-1">
+                <div class="container px-4 px-sm-5 text-dark text-center">
+                <div class="row row-cols-2 row-cols-sm-4 g-4">
+                    <div class="col-6">
+                        <a href="" style="text-decoration: none; color: black; font-weight: bold;">
+                            <div class="card" style="width:;">
+                                <img src="img/attendance.jpg" class="card-img-top" alt="...">
+                                <p class="card-title">Attendance</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6">
+                        <a href="" style="text-decoration: none; color: black; font-weight: bold;">
+                            <div class="card" style="width:">
+                                <img src="img/quizzes.jpg" class="card-img-top" alt="...">
+                                <p class="card-title">Quizzes</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6">
+                        <a href="" style="text-decoration: none; color: black; font-weight: bold;">
+                            <div class="card" style="width:">
+                                <img src="img/recitation.jpg" class="card-img-top" alt="...">
+                                <p class="card-title">Recitation</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6">
+                        <a href="" style="text-decoration: none; color: black; font-weight: bold;">
+                            <div class="card" style="width:">
+                                <img src="img/grades.jpg" class="card-img-top" alt="...">
+                                <p class="card-title">Quarterly Grades</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </section>
+            </div>
+        </main>
+        <footer>
+            <div class="text-white-50 text-center mt-5 pt-5">
+                <p>&copy; GradeSys 2023</p>
+            </div>
+        </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
+</html>
